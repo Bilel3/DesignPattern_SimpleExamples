155cfb0d84db0aa39ba647a86c363bcf82e98bd0
unknown <bayoudhibilel@gmail.com>
2020-07-20 01:39:37 +0100

    fix cercle and square classes

--- src/p1/designPattern/abstractfactory/Square.java
+++ src/p1/designPattern/abstractfactory/Square.java
@@ -2,6 +2,6 @@ package p1.designPattern.abstractfactory;
 
 public class Square implements Shape{
 	public void draw() {
-		System.out.println("Inside RoundedSquare::draw() method.");
+		System.out.println("Inside Square::draw() method.");
 	}
 }
